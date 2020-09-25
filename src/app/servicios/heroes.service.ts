fa8ff0a6e4cf07a69072b9209f8620400726688d
Jonathan <mcjhona069@gmail.com>
2020-09-24 20:50:39 -0500

    Buscador de heroes

--- src/app/servicios/heroes.service.ts
+++ src/app/servicios/heroes.service.ts
@@ -69,6 +69,20 @@ export class HeroesService{
       return this.heroes[idx];
     }
 
+
+    buscarHeroes( termino:string){
+
+      let heroesArr:Heroe[] = [];
+      termino = termino.toLowerCase();
+
+      for( let heroe of this.heroes ){
+        let nombre = heroe.nombre.toLowerCase();
+        if (nombre.indexOf( termino ) >=0){
+          heroesArr.push( heroe )
+        }
+      }
+      return heroesArr;
+    }
 }
 
 export interface Heroe{
